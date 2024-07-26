a90fd57287733349591cf4afa571541d2cc417ec
NISHAD  BHOYAR <119220061+WhiteDevil2326@users.noreply.github.com>
2024-07-26 16:00:02 +0530

    Add files via upload

---
+++ MERN_STACK_RESTAURANT_RESERVATION-main/MERN_STACK_RESTAURANT_RESERVATION-main/frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Nishad</div>
+          <div className="right">
+            <p>seven11</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Nishad bhoyar</p>
+          </div>
+          <div className="right">
+            <p>Nishad bhoyar.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
